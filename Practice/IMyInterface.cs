0c5d0cc26567d7cf6b381b985449386a341fd3fa
Shahzaib Ahmed Khan <shahzaib.ahmed@hyperdatacomputing.com>
2025-06-30 19:58:47 +0500

    Add project files.

---
+++ Practice/IMyInterface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class IMyInterface
+    {
+        public void Greet()
+        {
+            Console.WriteLine("Hello from IMyInterface!");
+        }
+    }
+    public class MyClass : IMyInterface
+    {
+        public  void Greet()
+        {
+            Console.WriteLine("Hello from My Class!");
+        }
+    }
+    public class MyClass2 : IMyInterface
+    {
+        
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,17 @@
+﻿using Practice;
+
+public class Program
+{
+    //interface and defaault implementation
+    public static void Main()
+    {
+        MyClass obj = new MyClass();
+        obj.Greet();
+        MyClass2 obj2 = new MyClass2();
+        obj2.Greet();
+
+        IMyInterface iobj = new MyClass2();
+        iobj.Greet();
+
+    }
+}
